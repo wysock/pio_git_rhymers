85a0cac6dfb8859782147a9f3cc38e20c6416074
wysock <222619@edu.p.lodz.pl>
2021-01-19 22:13:35 +0100

    1.3 poprawione formatowanie i komenatrz

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
@@ -10,8 +10,11 @@ int totalRejected = 0;
 
 	public void countIn(int in) {
 	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
+		totalRejected++;
+	else
+		super.countIn(in);
 	}
 }
+
+
+// kombinacja klawiszowa alt + ->/<-   pozwala na przeskakiwanie po otwartych plikach
